6b3734c40cc90d0b1449c96ff61bf9635b1a2158
crblazquez <62717643+crblazquez@users.noreply.github.com>
2020-03-26 22:03:23 +0100

    Update CallsPageTranslation.jsx
    Übersetzung fertig

--- src/App/CallsPage/CallsPageTranslation.jsx
+++ src/App/CallsPage/CallsPageTranslation.jsx
@@ -3,6 +3,9 @@ export const CallsPageTranslation = {
     serverOffline: {
         english: "Server currently offline. Please try again later.",
         deutsch: "Server zurzeit offline. Bitte versuchen Sie es später noch einmal.",
+        español: "El servidor está actualmente sin conexión. Por favor, inténtelo de nuevo más tarde.",
+        français: "Serveur actuellement hors ligne. Veuillez réessayer plus tard.",
+        italiano: "Server attualmente offline. Si prega di riprovare più tardi.",
     },
     noNewCalls: {
         english: "Currently no new calls. Please try again in a few minutes.",
@@ -11,30 +14,51 @@ export const CallsPageTranslation = {
     acceptCall: {
         english: "Accept Call",
         deutsch: "Anruf Annehmen",
+        español: "Responder a la llamada",
+        français: "Répondre à l'appel",
+        italiano: "Rispondere alla chiamata",
     },
     online: {
         english: "Online",
         deutsch: "Online",
+        español: "En línea",
+        français: "En ligne",
+        italiano: "Online",
     },
     offline: {
         english: "Offline",
         deutsch: "Offline",
+        español: "Sin conexión",
+        français: "Hors de ligne",
+        italiano: "Offline",
     },
     acceptedCalls: {
         english: "Accepted Calls",
         deutsch: "Angenommene Anrufe",
+        español: "Llamadas respondidas",
+        français: "Appels reçus",
+        italiano: "Chiamate ricevute",
     },
     noAcceptedCalls: {
         english: "No Accepted Calls",
         deutsch: "Keine Angenommenen Anrufe",
+        español: "Ninguna llamada respondida",
+        français: "Pas d'appels répondus",
+        italiano: "Nessuna chiamata risposta",
     },
     fulfilledCalls: {
         english: "Fulfilled Calls",
         deutsch: "Abgeschlossene Anrufe",
+        español: "Llamadas concluidas",
+        français: "Appels terminés",
+        italiano: "Chiamate completate",
     },
     noFulfilledCalls: {
         english: "No Fulfilled Calls",
         deutsch: "Keine Abgeschlossenen Anrufe",
+        español: "Ninguna llamada concluida",
+        français: "Pas d'appels terminés",
+        italiano: "Nessuna chiamata completata",
     },
 };
 
