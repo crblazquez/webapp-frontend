127c9753c34493636f0111f5fafab963af84c875
crblazquez <62717643+crblazquez@users.noreply.github.com>
2020-03-26 21:33:51 +0100

    Update NavbarTranslation.jsx
    Übersetzung fertig

--- src/App/Navbar/NavbarTranslation.jsx
+++ src/App/Navbar/NavbarTranslation.jsx
@@ -3,38 +3,65 @@ export const NavbarTranslation = {
     guide: {
         english: "Guide",
         deutsch: "Ratgeber",
+        español: "Guía",
+        français: "Guide",
+        italiano: "Guida",
     },
     calls: {
         english: "Calls",
         deutsch: "Anrufe",
+        español: "Llamadas",
+        français: "Appels",
+        italiano: "Chiamate",
     },
     account: {
         english: "Account",
         deutsch: "Benutzerkonto",
+        español: "Cuenta",
+        français: "Compte",
+        italiano: "Conto",
     },
     login: {
         english: "Login",
         deutsch: "Einloggen",
+        español: "Iniciar sesión",
+        français: "Connexion",
+        italiano: "Accedere",
     },
     logout: {
         english: "Logout",
         deutsch: "Ausloggen",
+        español: "Cerrar sesión",
+        français: "Déconnecter",
+        italiano: "Disconnettare",
     },
     logoutQuestion: {
         english: "Are your Sure?",
         deutsch: "Sind Sie Sicher?",
+        español: "¿Está seguro?",
+        français: "Êtes-vous sûr ?",
+        italiano: "È sicuro?",
     },
     cancel: {
         english: "Cancel",
         deutsch: "Abbrechen",
+        español: "Cancelar",
+        français: "Annuler",
+        italiano: "Cancellare",
     },
     menu: {
         english: "Menu",
         deutsch: "Menü",
+        español: "Menú",
+        français: "Menu",
+        italiano: "Menu",
     },
     language: {
         english: "Language",
-        deutsch: "Sprache"
+        deutsch: "Sprache",
+        español: "Idioma",
+        français: "Langue",
+        italiano: "Lingua",
     }
 };
 
