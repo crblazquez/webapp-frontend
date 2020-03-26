ff507d28d98787b6e5725fb98f58d5712e5df896
crblazquez <62717643+crblazquez@users.noreply.github.com>
2020-03-26 21:53:17 +0100

    Update AccountPageTranslation.jsx
    Übersetzung fertig

--- src/App/AccountPage/AccountPageTranslation.jsx
+++ src/App/AccountPage/AccountPageTranslation.jsx
@@ -4,62 +4,113 @@ export const AccountPageTranslation = {
     passwordConfirmationMatch: {
         english: "\"New Password\" and \"New Password Confirmation\" do not match.",
         deutsch: "\"Neues Passwort\" und \"Bestätigung Neues Passwort\" sind unterschiedlich.",
+        español: "La \"Nueva contraseña\" y la \"Confirmación de la nueva contraseña\" son distintas.",
+        français: "Le \"Nouveau mot de passe\" et la \"Confirmation du nouveau mot de passe\" sont différents",
+        italiano: "La \"Nuova password\" e la \"Conferma  della nuova password\" sono diverse.",
+
     },
     passwordTooShort: {
         english: "\"New Password\" too short (at least 8 characters).",
         deutsch: "\"Neues Passwort\" zu kurz (mindestens 8 Zeichen).",
+        español: "La \"Nueva contraseña\" es demasiado corta (al menos 8 caracteres).",
+        français: "Le \"nouveau Mot de passe\" es trop court (au moins 8 caractères).",
+        italiano: "La \"Nuova passwort\" è troppo corta (almeno 8 caratteri).",
+        
+
     },
     passwordFieldEmpty: {
         english: "Please fill in all three password fields.",
         deutsch: "Bitte füllen Sie alle drei Passwort Felder aus.",
+        español: "Por favor, rellene los tres campos de contraseña.",
+        français: "S'il vous plaît, remplissez les trois champs du mot de passe.",
+        italiano: "Perfavore. Compili i tre campi della password.",
     },
     serverOffline: {
         english: "Server currently offline. Please try again later.",
         deutsch: "Server zurzeit offline. Bitte versuchen Sie es später noch einmal.",
+        español: "El servidor está actualmente sin conexión. Por favor, inténtelo de nuevo más tarde.",
+        français: "Serveur actuellement hors ligne. Veuillez réessayer plus tard.",
+        italiano: "Server attualmente offline. Si prega di riprovare più tardi.",
+
     },
     resendVerification: {
         english: "Resend Verification",
         deutsch: "Bestätigung Erneut Senden",
+        español: "Reenviar confirmación",
+        français: "Renvoyer la confirmation",
+        italiano: "Inviare nuovamente la conferma",
+        
     },
     changePassword: {
         english: "Change Password",
         deutsch: "Passwort Ändern",
+        español: "Cambiar la contraseña",
+        français: "Changer le mot de passe",
+        italiano: "Cambiare la password",
     },
     cancel: {
         english: "Cancel",
         deutsch: "Abbrechen",
+        español: "Cancelar",
+        français: "Annuler",
+        italiano: "Cancellare",
     },
     submit: {
         english: "Submit",
         deutsch: "Übernehmen",
+        español: "Aceptar",
+        français: "Accepter",
+        italiano: "Accettare",
     },
     email: {
         english: "Email",
         deutsch: "Email",
+        español: "Correo electrónico",
+        français: "E-mail",
+        italiano: "E-mail",
     },
     zipCode: {
         english: "Zip Code",
         deutsch: "Postleitzahl",
+        español: "Código postal",
+        français: "Code postal",
+        italiano: "Codice postale",
     },
     city: {
         english: "City",
         deutsch: "Stadt",
+        español: "Ciudad",
+        français: "Ville",
+        italiano: "Città",
     },
     country: {
         english: "Country",
         deutsch: "Land",
+        español: "País",
+        français: "Pays",
+        italiano: "Paese",
     },
     oldPassword: {
         english: "Old Password",
         deutsch: "Altes Passwort",
+        español: "Antigua contraseña",
+        français: "Ancien mot de passe",
+        italiano: "Vecchia password",
     },
     newPassword: {
         english: "New Password",
         deutsch: "Neues Passwort",
+        español: "Nueva contraseña",
+        français: "Nouveau mot de passe",
+        italiano: "Nuova password",
     },
     newPasswordConfirmation: {
         english: "New Password Confirmation",
         deutsch: "Bestätigung Neues Passwort",
+        español: "Confirmación de la nueva contraseña",
+        français: "Confirmation du nouveau mot de passe",
+        italiano: "Conferma della nuova password",
+        
     },
 };
 
