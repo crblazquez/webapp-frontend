b04386b9e75c3efac9eb1f009a6aeff514ab08fe
crblazquez <62717643+crblazquez@users.noreply.github.com>
2020-03-27 15:27:42 +0100

    Update GuidePageTranslation.jsx
    Spanisch und italienisch fertig, französisch kommt bald

--- src/App/GuidePage/GuidePageTranslation.jsx
+++ src/App/GuidePage/GuidePageTranslation.jsx
@@ -6,6 +6,8 @@ export const GuidePageTranslation = {
     guideTitle: {
         english: (<React.Fragment>Guide <em>(coming soon)</em></React.Fragment>),
         deutsch: (<React.Fragment>Ratgeber <em>(kommt bald)</em></React.Fragment>),
+        español: (<React.Fragment>Guía <em>(próximamente)</em></React.Fragment>),
+        italiano: (<React.Fragment>Guida <em>(prossimamente)</em></React.Fragment>),
     },
     text1: {
         english: (
@@ -49,26 +51,78 @@ export const GuidePageTranslation = {
                 Bitte benutzt diese Seite in Zukunft, damit wir einen guten, regulierten Service garantieren können!
             </React.Fragment>
         ),
+        español: (
+            <React.Fragment>
+                ¡Los colaboradores voluntarios que contestan al teléfono no deberían estar solos al realizar su tarea!
+
+                Esta página web contendrá una <strong>Lista de consejos</strong> para <strong>todas las cuestiones posibles </strong>
+                que nos puedan plantear los voluntarios.
+
+                <br/><br/>
+
+                Como no queremos difundir <strong>ningún tipo de verdades a medias</strong>, sobre todo en cuanto al comportamiento
+                observado en un pequeño grupo de personas y en algún caso de estado mental inusual, aún no hemos activado
+                esta página web.
+
+                Actualmente estamos trabajando en las mejores estrategias y consejos que cuenten con una amplia aprobación.
+
+                <br/><br/>
+
+                Por favor, sírvase de esta página en el futuro para que nosotros podamos garantizar un buen servicio regulado.
+            </React.Fragment>
+        ),
+        italiano: (
+            <React.Fragment>
+                I volontari che rispondono al telefono non dovrebbero essere soli nel loro impegno!
+
+                Questo sito web conterrà una <strong>lista di suggerimenti</strong> per <strong>tutte le possibili domande</strong> 
+                che i volontari possono avere.
+
+                <br/><br/>
+
+                Poiché non vogliamo diffondere <strong>nessun tipo di mezze verità</strong>, soprattutto per quanto riguarda 
+                il comportamento osservato in un piccolo gruppo di persone e in alcuni casi di stato mentale insolito, non abbiamo 
+                ancora attivato questo sito web.
+
+                Al momento stiamo lavorando alle migliori strategie e ai migliori consigli che incontreranno un ampio consenso.
+
+                <br/><br/>
+
+                La preghiamo di utilizzare questa pagina in futuro, in modo da poter garantire un buon servizio regolamentato.
+            </React.Fragment>
+        ),
+
     },
     groceryShopping: {
         english: (<React.Fragment>Buying groceries/everyday things</React.Fragment>),
         deutsch: (<React.Fragment>Lebensmittel/Alltägliches einkaufen</React.Fragment>),
+        español: (<React.Fragment>Alimentos/Compras del día a día</React.Fragment>),
+        italiano: (<React.Fragment>Cibo/Acquisti quotidiani</React.Fragment>),
     },
     pickingUpParcels: {
         english: (<React.Fragment>Picking up parcels from the post office</React.Fragment>),
         deutsch: (<React.Fragment>Pakete/Sendungen von der Post abholen</React.Fragment>),
+        español: (<React.Fragment>Recoger los paquetes/envíos de la oficina de correos</React.Fragment>),
+        italiano: (<React.Fragment>Ritirare i pacchi/gli invii presso l'ufficio postale</React.Fragment>),
     },
     storesAreClosed: {
         english: (<React.Fragment>Stores are closed/risky: Helping with internet shopping</React.Fragment>),
         deutsch: (<React.Fragment>Fachgeschäfte geschlossen/riskant: Hilfe beim Internetkauf</React.Fragment>),
+        español: (<React.Fragment>Tiendas especializadas cerradas/supone un riesgo: Ayuda con las compras por Internet</React.Fragment>),
+        italiano: (<React.Fragment>Negozi specializzati chiusi/rischio: Aiuto per lo shopping su Internet</React.Fragment>),
     },
     pickingUpMedication: {
         english: (<React.Fragment>Picking up prescription/medication</React.Fragment>),
         deutsch: (<React.Fragment>Rezepte/Arzneimittel besorgen</React.Fragment>),
+        español: (<React.Fragment>Recoger recetas/medicamentos</React.Fragment>),
+        italiano: (<React.Fragment>Ottenere prescrizioni/farmaci</React.Fragment>),
+        
     },
     mentalHealth: {
         english: (<React.Fragment>Dealing with mental health</React.Fragment>),
         deutsch: (<React.Fragment>Tipps und Vermittlung bei Seelsorge</React.Fragment>),
+        español: (<React.Fragment>Consejos para lidiar con la salud mental</React.Fragment>),
+        italiano: (<React.Fragment> Consigli per trattare con la salute mentale</React.Fragment>),
     },
     text2: {
         english: (
@@ -91,6 +145,30 @@ export const GuidePageTranslation = {
                 <br/><br/>
 
                 Wir brauchen deine Hilfe bei: <strong>Übersetzung, Sprach-Synchronisation, Service-Strategien Ausarbeiten</strong> und vielem mehr!
+            </React.Fragment>
+        ),
+        español: (
+            <React.Fragment>
+                Si quieres <strong>colaborar con nosotros de manera activa</strong> puede contactarnos vía:
+                <br/>
+                <a href="mailto:jobs@hilfe-am-ohr.de">jobs@hilfe-am-ohr.de</a>
+
+                <br/><br/>
+
+                Necesitamos tu ayuda en los siguientes ámbitos: <strong>Traducción, doblaje, Desarrollo de estrategias de servicio</strong>
+                ¡y mucho más!
+            </React.Fragment>
+        ),
+        italiano: (
+            <React.Fragment>
+                Se volete <strong>collaborare con noi in modo attivo</strong>, potete contattarci via:
+                <br/>
+                <a href="mailto:jobs@hilfe-am-ohr.de">jobs@hilfe-am-ohr.de</a>
+
+                <br/><br/>
+
+                Abbiamo bisogno del tuo aiuto nei seguenti settori: <strong>Traduzione, doppiaggio, sviluppo di strategie di servizio</strong> 
+                e molto altro ancora!
             </React.Fragment>
         ),
     },
