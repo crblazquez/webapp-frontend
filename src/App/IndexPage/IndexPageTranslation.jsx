deb82d025edffbe5b3e610534bddeb1f889e8aae
crblazquez <62717643+crblazquez@users.noreply.github.com>
2020-03-27 14:50:15 +0100

    Update IndexPageTranslation.jsx
    Spanisch und Italienisch fertig. Französisch kommt bald.

--- src/App/IndexPage/IndexPageTranslation.jsx
+++ src/App/IndexPage/IndexPageTranslation.jsx
@@ -6,6 +6,8 @@ export const IndexPageTranslation = {
     indexSubtitle: {
         english: (<React.Fragment>A hotline for people without internet access</React.Fragment>),
         deutsch: (<React.Fragment>Eine Hotline für Menschen ohne Internetzugang</React.Fragment>),
+        español: (<React.Fragment>Una línea directa para personas sin acceso a internet</React.Fragment>),
+        italiano: (<React.Fragment>Una Hotline per persone senza accesso ad Internet</React.Fragment>),
     },
     text1: {
         english: (
@@ -16,6 +18,16 @@ export const IndexPageTranslation = {
         deutsch: (
             <React.Fragment>
                 Rufe jetzt an: <a href="tel:+49-30-2555-5305"><strong>+49 30 2555 5305</strong></a>
+            </React.Fragment>
+        ),
+        español: (
+            <React.Fragment>
+                Llame ahora: <a href="tel:+49-30-2555-5305"><strong>+49 30 2555 5305</strong></a>
+            </React.Fragment>
+        ),
+        italiano: (
+            <React.Fragment>
+                Chiama ora: <a href="tel:+49-30-2555-5305"><strong>+49 30 2555 5305</strong></a>
             </React.Fragment>
         ),
     },
@@ -30,6 +42,16 @@ export const IndexPageTranslation = {
                 <em>Showcase kommt bald!</em>
             </React.Fragment>
         ),
+        español: (
+            <React.Fragment>
+                <em>¡Enseguida una demostración!</em>
+            </React.Fragment>
+        ),
+        italiano: (
+            <React.Fragment>
+                <em>Una proba subito!</em>
+            </React.Fragment>
+        ),
     },
     text3: {
         english: (
@@ -42,18 +64,34 @@ export const IndexPageTranslation = {
                 Schaut euch alle Code Repositories auf <a rel="noopener noreferrer" target="_blank" href="https://github.com/Hilfe-am-Ohr">GitHub</a> an
             </React.Fragment>
         ),
+        español: (
+            <React.Fragment>
+                Consulte todos los repositorios de códigos en <a rel="noopener noreferrer" target="_blank" href="https://github.com/Hilfe-am-Ohr">GitHub</a> an
+            </React.Fragment>
+        ),
+        italiano: (
+            <React.Fragment>
+                Tutti i codici dai repository possono essere consultati qui: <a rel="noopener noreferrer" target="_blank" href="https://github.com/Hilfe-am-Ohr">GitHub</a> an
+            </React.Fragment>
+        ),
     },
     readOurGuide: {
         english: "Read our Guide",
         deutsch: "Lest unseren Guide",
+        español: "Lea nuestra guía",
+        italiano: "Leggere la nostra guida",
     },
     register: {
         english: "Register",
         deutsch: "Registrieren",
+        español: "Registrarse",
+        italiano: "Registrarsi",
     },
     text4: {
         english: "How can you support us?",
         deutsch: "Wie könnt ihr uns unterstützen?",
+        español: "¿Cómo puedo ayudarle?",
+        italiano: "Come posso aiutarla?",
     },
     text5: {
         english: (
@@ -102,6 +140,52 @@ export const IndexPageTranslation = {
                 4. Nehmt an unserer <strong>Umfrage</strong> teil, indem ihr die Hotline anruft <em>(~ 1 Minute)</em>
             </React.Fragment>
         ),
+        español: (
+            <React.Fragment>
+                1. Dale a 'me gusta' a nuestra idea - que ha visto arriba - en&nbsp;
+                <a href="https://www.youtube.com/watch?v=veRkcaPmV4g"
+                   rel="noopener noreferrer"
+                   target="_blank"><strong>YouTube</strong></a>
+
+                <br/><br/>
+
+                2. Dale a 'me gusta' a nuestro proyecto en&nbsp;
+                <a href="https://devpost.com/software/hilfehotline-finde-einfach-helfende"
+                   rel="noopener noreferrer"
+                   target="_blank"><strong>DevPost</strong></a>
+
+                <br/><br/>
+
+                3. <strong>Regístrate como voluntario</strong> en esta página
+
+                <br/><br/>
+
+                4. Participa en nuestra <strong>encuesta</strong>, llamando a la línea directa <em>(~ 1 Minuto)</em>
+            </React.Fragment>
+            ),
+        italiano: (
+            <React.Fragment>
+                1. Metti 'mi piace' alla nostra idea - che ha visto sopra - su&nbsp;
+                <a href="https://www.youtube.com/watch?v=veRkcaPmV4g"
+                   rel="noopener noreferrer"
+                   target="_blank"><strong>YouTube</strong></a>
+
+                <br/><br/>
+
+                2. Metti 'mi piace' al nostro progetto su&nbsp;
+                <a href="https://devpost.com/software/hilfehotline-finde-einfach-helfende"
+                   rel="noopener noreferrer"
+                   target="_blank"><strong>DevPost</strong></a>
+
+                <br/><br/>
+
+                3. <strong>Registrati come volontario</strong> su questa pagina
+
+                <br/><br/>
+
+                4. Partecipa al nostro <strong>sondaggio</strong>, chiamando la hotline <em>(~ 1 Minuto)</em>
+            </React.Fragment>
+        ),
     },
     text6: {
         english: (
@@ -114,6 +198,18 @@ export const IndexPageTranslation = {
             <React.Fragment>
                 <em>Job-Pinnwand kommt bald!</em>&nbsp;
                 Kontaktiert uns über <a href="mailto:jobs@hilfe-am-ohr.de">info@hilfe-am-ohr.de</a>
+            </React.Fragment>
+        ),
+        español: (
+            <React.Fragment>
+                <em>¡El tablón de anuncios estará disponible próximamente!</em>&nbsp;
+                Contáctenos vía <a href="mailto:jobs@hilfe-am-ohr.de">info@hilfe-am-ohr.de</a>
+            </React.Fragment>
+        ),
+        italiano: (
+            <React.Fragment>
+                <em>Una bacheca sarà disponibile presto!</em>&nbsp;
+                Contattateci via <a href="mailto:jobs@hilfe-am-ohr.de">info@hilfe-am-ohr.de</a>
             </React.Fragment>
         ),
     },
